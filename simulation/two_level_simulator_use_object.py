f9b5577a0dab6dc2a6060abbde41a4b6a5f4ee43
sijinwnag <63268286+sijinwnag@users.noreply.github.com>
2023-01-22 13:29:44 +1100

    s

---
+++ simulation/two_level_simulator_use_object.py
@@ -0,0 +1,41 @@
+# %% imports
+from two_level_generator_object import *
+
+# define the object
+# simulator = two_level_lifetime_generator()
+
+# %%simulate the data with a range of Et1
+
+# swtip Et2 from -0.5 to 0.5 eV
+# for central_Et1 in np.arange(-0.5, 0.6, 0.1):
+#     print('up to ' + str(central_Et1))
+#     # define the object
+#     simulator = two_level_lifetime_generator()
+#     # define the simulation range
+
+#     simulator.PARAM['Et_min_1'] = central_Et1 - 0.05
+#     simulator.PARAM['Et_max_1'] = central_Et1 + 0.05
+#     # define the file name
+#     simulator.NAME = 'multi_level_L' + 'Et1' + str(central_Et1)
+#     simulator.mixture_simulate()
+
+# swtip Et2 from -0.5 to 0.5 eV
+for central_Et2 in np.arange(-0.5, 0.6, 0.1):
+    print('up to ' + str(central_Et2))
+    # define the object
+    simulator = two_level_lifetime_generator()
+    # define the simulation range
+
+    simulator.PARAM['Et_min_2'] = central_Et2 - 0.05
+    simulator.PARAM['Et_max_2'] = central_Et2 + 0.05
+    # define the file name
+    simulator.NAME = 'multi_level_L' + 'Et1' + str(central_Et2)
+    simulator.mixture_simulate()
+
+# %% simulate the data updating the datasize
+# simulator = two_level_lifetime_generator()
+# simulator.PARAMETERS['n_defects']=80000
+# simulator.mixture_simulate()
+
+# %%
+simulator.email_reminder()
